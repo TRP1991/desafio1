95695524caa20204657eab52f471ea6efd394128
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-04 00:40:19 -0400

    Star solution program Desafio1

---
+++ desafio_1.rb
@@ -0,0 +1,17 @@
+=begin 
+
+Solución Desafío 1.
+
+Dado el array:
+
+a = [1, 9 ,2, 10, 3, 7, 4, 6]
+
+1. Utilizando map sumar uno a cada uno de los valores del array.
+
+Utilizando map convertir todos los valores a float.
+Utilizando select descartar todos los elementos menores a 5 en el array.
+Utilizando inject sumar todos los valores del array.
+Utilizando .count contar todos los elementos menores que 5.
+
+=end 
+
